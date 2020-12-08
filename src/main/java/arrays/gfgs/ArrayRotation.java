f557387df706b7532449f43cee3d859bf22ffbce
sarath <sarathtchander@gmail.com>
2020-12-08 21:32:26 +0530

    array rotation 02

--- src/main/java/arrays/gfgs/ArrayRotation.java
+++ src/main/java/arrays/gfgs/ArrayRotation.java
@@ -2,10 +2,10 @@ package arrays.gfgs;
 
 import java.util.Arrays;
 
-/*
- *  Rotate an array of size n for d elements
- *  https://www.geeksforgeeks.org/array-rotation/
- * */
+/**
+ * Rotate an array of size n for d elements
+ * https://www.geeksforgeeks.org/array-rotation/
+ */
 public class ArrayRotation {
 
     public static void main(String[] args) {
@@ -13,6 +13,8 @@ public class ArrayRotation {
         String[] arr = {"1", "2", "3", "4", " 5", "6", " 7 "};
         System.out.println(Arrays.toString(rotateArray.rotateUsingTempArray(arr, 1)));
         System.out.println(Arrays.toString(rotateArray.leftRotateOneByOne(arr, 1)));
+        System.out.println(Arrays.toString(rotateArray.jugglingAlgorithmArrayRotation(arr, 3)));
+        System.out.println(Arrays.toString(rotateArray.reversalAlgorithm(arr, 10)));
     }
 
 
@@ -65,6 +67,66 @@ public class ArrayRotation {
                 array[i] = array[i + 1];
             }
             array[i] = tmp;
+        }
+
+        /**
+         *
+         */
+        public String[] jugglingAlgorithmArrayRotation(String[] array, int d) {
+            int n = array.length;
+            d = d % n;
+            int i, j, k;
+            String temp;
+            int gcd = gcd(d, n);
+            for (i = 0; i < gcd; i++) {
+                temp = array[i];
+                j = i;
+                while (true) {
+                    k = j + d;
+                    if (k >= n)
+                        k = k - n;
+                    if (k == i)
+                        break;
+                    array[j] = array[k];
+                    j = k;
+                }
+                array[j] = temp;
+            }
+            return array;
+        }
+
+        /**
+         * gcd based on euclid
+         */
+        int gcd(int a, int b) {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+
+        /**
+         * split the array[0 to d-1] and array[d to n-1]
+         * revere each set ones and finally reverse completely one more time totally 3 reverse
+         */
+        public String[] reversalAlgorithm(String[] array, int d) {
+            // if d greater than array length
+            d = d % array.length;
+            reverseArray(array, 0, d - 1);
+            reverseArray(array, d, array.length - 1);
+            reverseArray(array, 0, array.length - 1);
+            return array;
+        }
+
+        /**
+         * Just swaps the element of array till the middle
+         */
+        void reverseArray(String[] array, int start, int end) {
+            String temp;
+            while (start < end) {
+                temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
         }
 
 
